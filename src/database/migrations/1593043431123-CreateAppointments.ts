37bfcc078d91ecb18137627991085d6fd4addbc4
Maico Alexandre Kley <maicokley@hotmail.com>
2020-06-25 21:47:22 -0300

    refactor changing uuid to type of id

--- src/database/migrations/1593043431123-CreateAppointments.ts
+++ src/database/migrations/1593043431123-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593043431123
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1593049126088-CreateUsers.ts
+++ src/database/migrations/1593049126088-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1593049126088 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
