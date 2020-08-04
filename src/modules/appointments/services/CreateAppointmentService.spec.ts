43c05a81ec92687fed97214003da772245d153d0
Maico Alexandre Kley <maicokley@hotmail.com>
2020-08-03 23:28:12 -0300

    feat appointment creation

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '1231231231',
       provider_id: '12312313213132',
     });
 
@@ -29,12 +30,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '1231231231',
       provider_id: '12312313213132',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '1231231231',
         provider_id: '12312313213132',
       }),
     ).rejects.toBeInstanceOf(AppError);
